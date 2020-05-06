3db80bec81ac44146eb0363a7ff866b1dd6da92a
Grant Yoshitsu <grantkoji@gmail.com>
2020-05-06 21:43:56 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,6 +3,6 @@
 def reverse_each_word(sentence)
   array_sentence = sentence.to_a
   array_reversed = []
-    array_sentence.map {|word| array_reversed << word.reverse}
+    array_sentence.each {|word| array_reversed << word.reverse}
   array_reversed.to_s
 end
